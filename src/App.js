a123a6cd559ae2e1bf5bf2a909e376565d7fd600
Karol W <karol.wichmann@Outlook.com>
2019-06-16 17:16:37 +0200

    fully functional todo app. added: submitting new tasks; clicking the task to remove it. TODO: add adequate icons to make the app intuitive.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
 import React, { Component } from "react";
 import Todos from "./todos";
+import AddTask from "./AddTask";
 class App extends Component {
   state = {
     todos: [
-      { id: 1, content: "buy batteries" },
-      { id: 2, content: "check Heat Signature" }
+      { id: 0, content: "buy batteries" },
+      // { id: 2, content: "check Heat Signature" }
     ]
   };
 
-  deleteTodo = (todoId) => {
+  deleteTodo = todoId => {
     let todos = this.state.todos.filter(todo => {
-      return todo.id !== todoId
+      return todo.id !== todoId;
     });
+    this.setState({
+      todos: todos
+    });
+  };
+
+  addTask = (newTask)=> {
+    newTask.id = this.state.todos.length + 1;
+    let todos = [...this.state.todos, newTask];
     this.setState({
       todos: todos
     })
   }
+
   render() {
     return (
-
       <div className="todo-App container">
         <h1 className="blue-text">
-          At the end of the day... can you say you're proud of yourself?
+          At the end of the day... can you say you're proud of yourself? 
         </h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTask addTask={this.addTask}/>
       </div>
     );
   }
