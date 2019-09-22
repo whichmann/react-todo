bafac1f72381cd6067e9496f33e7dbfc01368e9b
Karol W <karol.wichmann@Outlook.com>
2019-09-22 22:03:56 +0200

    Refactored main app.js file to utilize hooks approach

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Todos from "./todos";
 import AddTask from "./AddTask";
 import Navbar from "./components/Navbar";
@@ -7,50 +7,41 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Caller from "./components/Caller";
 
-class App extends Component {
-  state = {
-    todos: [
-      { id: 0, content: "buy batteries" }
-      // { id: 2, content: "check Heat Signature" }
-    ]
-  };
+function App() {
+  const [todos, setTodos] = useState([{ id: 0, content: "buy batteries" }]);
 
-  deleteTodo = todoId => {
-    let todos = this.state.todos.filter(todo => {
+  const deleteTodo = todoId => {
+    setTodos(todos.filter(todo => {
       return todo.id !== todoId;
-    });
-    this.setState({
-      todos: todos
-    });
+    }))
   };
 
-  addTask = newTask => {
-    newTask.id = this.state.todos.length + 1;
-    let todos = [...this.state.todos, newTask];
-    this.setState({
-      todos: todos
-    });
+  const addTask = newTask => {
+    newTask.id = todos.length + 1;
+    setTodos([...todos, newTask]);
   };
 
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="todo-App">
-          <Navbar />
-          <Route exact path="/About" component={About} />
-          <Route exact path="/Contact" component={Contact} />
-          <Route exact path="/Caller" component={Caller} />
-          <div className="container">
-            <h1>
-              At the end of the day... can you say you're proud of yourself?
-            </h1>
-            <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} key={this.state.todos.id}/>
-            <AddTask addTask={this.addTask} />
-          </div>
+  return (
+    <BrowserRouter>
+      <div className="todo-App">
+        <Navbar />
+        <Route exact path="/About" component={About} />
+        <Route exact path="/Contact" component={Contact} />
+        <Route exact path="/Caller" component={Caller} />
+        <div className="container">
+          <h1>
+            At the end of the day... can you say you're proud of yourself?
+          </h1>
+          <Todos
+            todos={todos}
+            deleteTodo={deleteTodo}
+            key={todos.id}
+          />
+          <AddTask addTask={addTask} />
         </div>
-      </BrowserRouter>
-    );
-  }
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
